9403f494ab9ed44d2bdf3f09bbcb7ff90a29130b
Joepapa101 <98859327+Joepapa101@users.noreply.github.com>
2023-05-16 12:04:20 +1200

    yup

--- mc_searchitem.py
+++ mc_searchitem.py
@@ -5,13 +5,27 @@ from mc_dictionary_v1 import monster_cards
 while True:
     searchcard = enterbox('Search any card from the library')
     if searchcard in monster_cards:
-        #this got a bit funky to program
-        msgbox(f"The {searchcard}combo exists")
-    else:
-        msgbox(f'The combo {searchcard} does not exist.')
+        msgbox(f"{searchcard} exists")
         break
+    else:
+        msgbox(f'The  {searchcard} does not exist.')
 
-editcard = buttonbox('Would you like to edit this card')
-
+editcard = buttonbox('Would you like to edit this card', choices=['Yes', 'No'])
+if editcard == 'Yes':
+    print(monster_cards[searchcard])
+    strengthinput = enterbox('Enter strength')
+    speedinput = enterbox('Enter speed')
+    stealthinput = enterbox('Enter stealth')
+    cunninginput = enterbox('Enter cunning')
+    del monster_cards[searchcard]
+    monster_cards[searchcard] = {
+        'strength': [strengthinput],
+        'speed': [speedinput],
+        'stealth': [stealthinput],
+        'cunning': [cunninginput]
+    }
+    print(monster_cards[searchcard])
+elif editcard == 'No':
+    print('ur mum')
 
 
