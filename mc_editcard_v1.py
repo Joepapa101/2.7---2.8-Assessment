f7b6d61b4da38c410385dbcacf0af8b6279b32cf
Joepapa101 <98859327+Joepapa101@users.noreply.github.com>
2023-05-16 12:03:43 +1200

    yup

---
+++ mc_editcard_v1.py
@@ -0,0 +1,4 @@
+from easygui import *
+from mc_dictionary_v1 import monster_cards
+
+useritem = enterbox('Enter the option you want to edit')
---
+++ mc_searchitem_v2.py
@@ -0,0 +1,24 @@
+from easygui import *
+from mc_dictionary_v1 import monster_cards
+
+
+while True:
+    searchcard = enterbox('Search any card from the library')
+    if searchcard in monster_cards:
+        msgbox(f"{searchcard} exists")
+        break
+    else:
+        msgbox(f'The  {searchcard} does not exist.')
+
+editcard = buttonbox('Would you like to edit this card', choices=['Yes', 'No'])
+if editcard == 'Yes':
+    print(monster_cards[searchcard])
+    ability = buttonbox('What abilities would you like to change', choices=['Strength', 'Speed', 'Stealth', 'Cunning'])
+    print(monster_cards[searchcard])
+    abilityamount = enterbox(f'Enter new value for {ability}')
+    monster_cards[searchcard][ability] = [abilityamount]
+    print(monster_cards[searchcard])
+elif editcard == 'No':
+    print('ur mum')
+
+
