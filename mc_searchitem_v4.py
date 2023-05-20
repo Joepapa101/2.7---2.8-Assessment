cce145e4ebca6488e943b3015d8890897ce2d0e9
Joepapa101 <98859327+Joepapa101@users.noreply.github.com>
2023-05-20 15:21:01 +1200

    nj

---
+++ mc_searchitem_v4.py
@@ -0,0 +1,28 @@
+from easygui import *
+from mc_dictionary_v1 import monster_cards
+
+while True:
+    searchcard = enterbox('Search any card from the library')
+    if searchcard in monster_cards:
+        editcard = buttonbox(f'{searchcard} has {monster_cards[searchcard]["strength"]} strength,\
+ {monster_cards[searchcard]["speed"]} speed, {monster_cards[searchcard]["stealth"]} stealth,\
+ {monster_cards[searchcard]["cunning"]} cunning. ', choices=['Edit', 'Exit'])
+        break
+    else:
+        msgbox(f'{searchcard} does not exist.')
+
+
+if editcard == 'Edit':
+    print(monster_cards[searchcard])
+    ability = buttonbox('What abilities would you like to change', choices=[f'Strength ({monster_cards[searchcard]["strength"]})\
+', f'Speed ({monster_cards[searchcard]["speed"]})', f'Stealth ({monster_cards[searchcard]["stealth"]})', f'Cunning ({monster_cards[searchcard]["cunning"]})'])
+    print(monster_cards[searchcard])
+    abilityamount = enterbox(f'Enter new value for {ability}')
+    realability = ability.split(' ')[0]
+    monster_cards[searchcard][realability.lower()] = [abilityamount]
+    print(monster_cards[searchcard])
+elif editcard == 'Exit':
+    print('ur mum')
+
+#firstcombo.split(':')[1]
+#Change-log - improved searchcard function
