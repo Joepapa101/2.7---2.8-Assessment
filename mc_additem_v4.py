d3f7e2d3070d61a28041dd3889312b7fcc052e10
Joepapa101 <98859327+Joepapa101@users.noreply.github.com>
2023-05-23 12:32:20 +1200

    nn

---
+++ mc_additem_v4.py
@@ -0,0 +1,42 @@
+from easygui import *
+from mc_dictionary_v1 import monster_cards
+
+
+while True:
+    cardname = enterbox('Enter the name of the card you would like to add')
+    
+    while True:
+        strength = int(enterbox('Enter the strength of the character'))
+        if strength >= 25:
+            msgbox("You can't enter a number greater than 25 strength")
+        else:
+            break
+    
+    while True:
+        speed = int(enterbox('Enter the speed of the character'))
+        if speed >= 25:
+            msgbox("You can't enter a number greater than 25 speed")
+        else:
+            break
+
+    while True:
+        stealth = int(enterbox('Enter the stealth of the character'))
+        if stealth >= 25:
+            msgbox("You can't enter a number greater than 25 stealth")
+        else:
+            break
+
+    while True:
+        cunning = int(enterbox('Enter the cunning of the character'))
+        if cunning >= 25:
+            msgbox("You can't enter a number greater than 25 cunning6")
+        else:
+            break
+
+
+    monster_cards[cardname] = {
+        'strength': [strength],
+        'speed': [speed],
+        'stealth': [stealth],
+        'cunning': [cunning]
+    }
